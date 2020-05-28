4083755abf7977ad57bafb97a384e412a8f5c7d7
Nimzyow <n_soufiani@hotmail.com>
2020-05-28 16:46:55 +0100

    can change name state dynamically

--- src/Person.js
+++ src/Person.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Person = (props) => {
-  return <div data-test="person-container">{props.children({})}</div>;
+  const [state, setState] = useState(props.initState);
+
+  const onChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div data-test="person-container">
+      {props.children({ state, onChange })}
+    </div>
+  );
 };
--- src/Person.test.js
+++ src/Person.test.js
@@ -4,6 +4,11 @@ import { shallow } from "enzyme";
 import { Person } from "./Person";
 
 describe("Person.js", () => {
+  let onSubmitSpy = jest.fn();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("renders person component correctly", () => {
     const wrapper = shallow(<Person>{() => {}}</Person>);
     const personContainer = wrapper.find('[data-test="person-container"]');
@@ -20,5 +25,32 @@ describe("Person.js", () => {
     const testContainer = wrapper.find('[data-test="test"]');
 
     expect(testContainer.length).toBe(1);
+  });
+  it("onChange changes value of name", () => {
+    const wrapper = shallow(
+      <Person initState={{ name: "" }}>
+        {({ state, onChange }) => {
+          return (
+            <input
+              data-test="text-test"
+              type="text"
+              name="name"
+              value={state.name || ""}
+              onChange={onChange}
+            />
+          );
+        }}
+      </Person>
+    );
+
+    let inputElement = wrapper.find('[data-test="text-test"]');
+
+    inputElement.simulate("change", {
+      target: { name: "name", value: "CHANGING THIS" },
+    });
+
+    inputElement = wrapper.find('[data-test="text-test"]');
+
+    expect(inputElement.prop("value")).toBe("CHANGING THIS");
   });
 });
